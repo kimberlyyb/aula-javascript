1672d5861c5e026bb227c2efa2d3f2e9b57dad1a
Kimberly Barros <kimberlyybarros@gmail.com>
2024-04-30 17:29:53 -0300

    comecei exercicio

---
+++ aula01/exercicio.js
@@ -0,0 +1,10 @@
+//propomt (classe)e alert(main)
+let numero1
+let numero2
+let soma
+
+numero1 = Number(prompt('Informe o número 1:'));
+numero2 = Number(prompt('Informe o número 2'));
+soma = numero1 + numero2
+
+alert(`A soma`)
