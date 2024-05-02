8fd796c0efbe89e9a9dc035e018e1e785bd15727
Kimberly Barros <kimberlyybarros@gmail.com>
2024-05-02 16:37:57 -0300

    split, replace

--- aula01/perguntas.js
+++ aula01/perguntas.js
@@ -2,4 +2,7 @@ let nome = prompt('Qual é o seu nome?')
 alert(`Olá, ${nome}!`)
 
 let idade = prompt('Qual a sua iade?')
- let bairro = prompt('Em que bairro você mora?')
+let bairro = prompt('Em que bairro você mora?')
+
+console.log(nome);
+console.log(nome.toLocaleUpperCase());
---
+++ aula02/main.js
@@ -0,0 +1,17 @@
+let frase = 'Adoro cachorro caramelo'
+let gato = 'gato'
+
+console.log(frase);
+
+console.log(frase.charAt(2));
+
+console.log(frase.length);
+
+console.log(gato.substring(1, 4));
+
+let fraseCortada = frase.split("cachorro")
+console.log(fraseCortada);
+console.log(fraseCortada.substring)
+
+console.log(frase);
+console.log(frase.replace('cachorro', 'gato'));
