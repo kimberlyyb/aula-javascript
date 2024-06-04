ad0df20aae4f46058da1c9a3fc1ac49e055e1bf0
Nathan Torres <nathansilvatorres@gmail.com>
2024-06-04 16:12:30 -0300

    addEventListenner

--- aula13/eventosDom.js
+++ aula13/eventosDom.js
@@ -2,7 +2,9 @@ const calcularMedia = () =>{
     let n1 = Number(document.querySelector('#nota1').value)
     let n2 = Number(document.querySelector('#nota2').value)
     let media = (n1 + n2)/2
+    let res = document.querySelector('#resultado')
+    res.innerHTML = (`Nota do aluno: ${media}`)
 }
-
-let res = document.querySelector('#resultado')
-res.innerHTML = (`Nota do aluno: ${media}`)
+  
+let calcular = document.querySelector("#botaoEnviar")
+calcular.addEventListener('click', calcularMedia)
