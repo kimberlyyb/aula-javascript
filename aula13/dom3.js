a49720c8733fe9018a398d6ae14d0a1603b51547
Nathan Torres <nathansilvatorres@gmail.com>
2024-06-04 15:49:14 -0300

    querySelector

---
+++ aula13/dom3.js
@@ -0,0 +1,11 @@
+let valor1 = document.getElementById("elemento1")
+elemento1.style.color = 'pink'
+
+let valor2 = document.querySelector("#p1")
+valor2.style.color = 'red'
+
+let valor3 = document.querySelectorAll('.p2')[0]
+valor3.style.color = 'blue'
+
+let valor4 = document.querySelectorAll('.p2')[1]
+valor4.style.color = 'blue'
